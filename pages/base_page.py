def2ec0a9bc4fc18ac25ebfd5b6ae237011d2b08
Larisa_Khisamova <Larisa_Khisamova@epam.com>
2024-02-13 13:49:18 +0100

    Add new methods

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,15 +1,35 @@
 import math
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
+
+from pages.locators import BasePageLocators
+
+
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
+    # def __init__(self, browser, url, timeout=10):
+    #     self.browser = browser
+    #     self.url = url
+    #     self.browser.implicitly_wait(timeout)
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
         print("Browser is open")
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+        # alert = self.browser.switch_to.alert
+        # alert.accept()
+        print("Can go to Login page")
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+        print("Login link is presented")
 
     def is_element_present(self, how, what):
         try:
@@ -18,6 +38,21 @@ class BasePage():
             print("Element is absent")
             return False
         print("Element is present")
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
         return True
 
     def solve_quiz_and_get_code(self):
