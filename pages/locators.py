80d8ec0883ca62d51e60d6a7dc53e2981f9e1583
Larisa_Khisamova <Larisa_Khisamova@epam.com>
2024-02-13 13:51:16 +0100

    Add class BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -4,9 +4,11 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators():
     LOGIN_FORM = (By.CLASS_NAME, "login_form")
     REGISTER_FORM = (By.CLASS_NAME, "register_form")
+
 
 class ProductPageLocators():
     CART_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
@@ -14,3 +16,9 @@ class ProductPageLocators():
     PRODUCT_NAME = (By.TAG_NAME, "h1")
     CART_COST = (By.CSS_SELECTOR, ".alertinner p strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div:nth-child(1)")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc") # негативный тест
